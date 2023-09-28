a6c8d8cbfb20490c4cb7e301ca793206e769511e
MihaelaBoian <pinzari_m@mail.ru>
2023-09-28 14:39:18 +0300

    k

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Defvariabile var = new Defvariabile();
+
+        System.out.println("Current World Population - "+var.wor_pop);
+
+        System.out.println("Cars Produced This Year - "+var.car_prod);
+
+        System.out.println("First Letter In Alphabet - "+var.f_letter);
+
+        System.out.println("Java is Amazing - "+var.java);
+
+        System.out.println("Pi - "+var.Pi);
+
+
+
     }
 }
