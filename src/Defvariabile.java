91381739620556d732efa526b010907032631bbe
MihaelaBoian <pinzari_m@mail.ru>
2023-09-28 14:38:27 +0300

    k

--- src/Defvariabile.java
+++ src/Defvariabile.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Defvariabile {
+public class Defvariabile {
+
+    long wor_pop = 7811080815L;
+
+    long car_prod = 55235358;
+
+    char f_letter = 'A';
+
+    boolean  java = true;
+
+    float Pi = 3.14159f;
+
 }
